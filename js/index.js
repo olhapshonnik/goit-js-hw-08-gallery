11bf3dd72fb54f69e67fec1ce55faee227783a9b
olhapshonnik <o.pshonnik@gmail.com>
2021-05-12 21:08:07 +0300

    добавления оверлей

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ import galleryItems from './gallery-items.js';
 const gallery = document.querySelector('.js-gallery');
 const lightbox = document.querySelector('.js-lightbox');
 
+const lightboxImage = document.querySelector('.lightbox__image');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+
+gallery.addEventListener('click', openModal);
+lightboxOverlay.addEventListener('click', closeModal);
+
+//создание галер
 const imageList = galleryItems
   .map(({ original, preview, description }) => {
     return `<li class="gallery__item">
@@ -19,8 +26,41 @@ const imageList = galleryItems
   .join('');
 
 gallery.insertAdjacentHTML('afterbegin', imageList);
-// const lightboxImage = document.querySelector('.lightbox__image');
-// const lightboxOverlay = document.querySelector('.lightbox__overlay');
+//
 
-// gallery.addEventListener('click', openModal);
-// lightboxOverlay.addEventListener('click', closeModal);
+
+
+//кнопка
+const btnClose = document.querySelector(
+  ".js-lightbox button[data-action='close-lightbox']",
+);
+//
+
+//
+btnClose.addEventListener('click', closeModal);
+
+function openModal(event) {
+  if (event.target.getAttribute('class') != 'gallery__image') {
+    return;
+  }
+  event.preventDefault();
+  lightbox.classList.add('is-open');
+  lightboxImage.src = event.target.dataset.source;
+  lightboxImage.alt = event.target.alt;
+}
+//
+
+//Закрытие модалки
+function closeModal({type, code}) {
+  if (type === 'keydown') {
+  } 
+  else {
+    clearImageAtributOnModal();
+  }
+}
+
+const clearImageAtributOnModal = () => {
+  lightbox.classList.remove('is-open');
+};
+//
+
